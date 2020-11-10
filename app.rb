f6203c9cdade6e54733a839bcb0cd0aed1ef8625
madelinemc <madeline.coons@gmail.com>
2020-11-10 16:28:24 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 class Application < Sinatra::Base
-  # Write your code here!
+  get '/' do
+    erb :index
+  end
 
+  post '/greet' do
+    erb :greet
+  end
+  
 end
+
+# ./app.rb is our main application file, defining the controller that will power this web application. 
+# We create an Application class and inherit from Sinatra::Base to give it all of the web superpowers 
+# needed to transform the standard Ruby class into a Sinatra controller.
+
+# Within our Application controller, we use the Sinatra DSL to create a GET route at the / URL. 
+# We tell our application to respond to HTTP GET requests to / by rendering the designated ERB template or HTML.
+
+# The line erb :index tells the application to render, or deliver to the user's browser, the file in views/index.erb. 
+# This is a Sinatra-provided functionality that renders ERB templates located in the views directory. 
--- spec/application_integration_spec.rb
+++ spec/application_integration_spec.rb
@@ -26,3 +26,23 @@ describe "POST '/greet' - User Greeting" do
     expect(page).to have_text("Hi Avi, nice to meet you!")
   end
 end
+
+# VISIT:
+# The visit method navigates the test's browser to a specific URL. 
+# It is equivalent to a user typing a URL into their browser's location bar. 
+# The argument it accepts is a string for the URL you want to test
+
+# PAGE: 
+# The page method in Capybara exposes the "session" or "browser" that is conceptually
+# (and literally) being used during the test. The page method gives you a 
+# Capybara::Session object that represents the browser page the user would actually 
+# be looking at if they navigated to '/' (or whichever route was last passed to visit).
+
+# As such, page responds to a lot of methods that represent actions a user 
+# could take on a page, such as click_link, fill_in, and body.
+
+# The page.body method will dump out all of the HTML in the current page as a string.
+
+# Capybara page objects respond to methods that relate intimately to HTML and the DOM 
+# (Document Object Model) that defines web applications.
+
